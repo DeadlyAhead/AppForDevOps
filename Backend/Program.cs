d4f4c2e997ac380be66070b317169ffe763d3201
ЯдерныйБобёр337 <holodkov.marat29@gmail.com>
2025-07-14 17:12:22 +0300

    shit for migrations

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -38,13 +38,13 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "To do list API v1"));
 
-// Database migration
-// using (var scope = app.Services.CreateScope())
-// {
-//     await Task.Delay(5000);
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     db.Database.Migrate();
-// }
+Database migration
+using (var scope = app.Services.CreateScope())
+{
+    await Task.Delay(5000);
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseRouting();
 app.UseCors("AllowAll");

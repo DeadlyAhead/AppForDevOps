fec93923fc2e21e6d0509fff439cc54fb7e68f4c
ЯдерныйБобёр337 <holodkov.marat29@gmail.com>
2025-07-15 19:00:57 +0300

    WTH migrations, was commd

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -33,12 +33,12 @@ app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "To do list API v1"));
 
 // Database migration
-using (var scope = app.Services.CreateScope())
-{
-    await Task.Delay(5000);
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+// using (var scope = app.Services.CreateScope())
+// {
+//     await Task.Delay(5000);
+//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     db.Database.Migrate();
+// }
 
 app.UseRouting();
 app.UseCors("AllowAll");

1476503c192eecba99d897d11ce56df8eb84a334
ЯдерныйБобёр337 <holodkov.marat29@gmail.com>
2025-08-26 12:25:53 +0300

    Added metric path

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -31,7 +31,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.UseSwagger();
-app.UseMetricServer();
+app.UseMetricServer(/api/metrics);
 app.UseHttpMetrics();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "To do list API v1"));
 
